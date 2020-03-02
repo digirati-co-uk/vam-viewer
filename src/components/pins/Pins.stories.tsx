257ab73e2116996326b456a5b97b10b90a7125b3
Kelsie <kelsie.braidwood@digirati.com>
2020-03-02 13:20:22 +0000

    Guided tour and pins folder structure framework

---
+++ src/components/pins/Pins.stories.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+export default { title: 'Pins Example| Pins' };
+
+export const Pins: React.FC = () => {
+  return <div>I will be a Pins example</div>;
+};
--- src/components/slideshow/Slideshow.stories.tsx
+++ src/components/slideshow/Slideshow.stories.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
-export default { title: 'CATEGORY LABEL| Slideshow' };
+export default { title: 'Slideshow| Slideshow' };
 
 export const Slideshow: React.FC = () => {
   return <div>I will be a slideshow example</div>;
