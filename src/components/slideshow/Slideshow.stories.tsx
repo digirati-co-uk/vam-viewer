fa0f275ad9bf5592890265aee475a79a5d071656
Kelsie <kelsie.braidwood@digirati.com>
2020-03-04 09:47:05 +0000

    Slideshow rendering in the storybook

--- src/components/slideshow/Slideshow.stories.tsx
+++ src/components/slideshow/Slideshow.stories.tsx
@@ -1,13 +1,18 @@
-import React from 'react';
+import React, { useRef } from 'react';
+//@ts-ignore
 import { SlideShow } from '../../canvasPanelComponents/slideshow/src/index.js';
 
 export default { title: 'Slideshow| Slideshow' };
 
 export const SlideshowStory: React.FC = () => {
+  const slideShowEl = useRef('slideShowEl');
+
   return (
-    <>
-      <div>BLAH</div>
-      <SlideShow />
-    </>
+    <div id="slideShowEl" style={{ height: '100vh' }}>
+      <SlideShow
+        element={slideShowEl}
+        manifestUri="https://raw.githubusercontent.com/4d4mm/adam-digirati.github.io/master/balenciaga4.json"
+      />
+    </div>
   );
 };
