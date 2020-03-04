dbfac4a46f06814c776766fa8e6b5d8b26e19d38
Kelsie <kelsie.braidwood@digirati.com>
2020-03-04 11:33:25 +0000

    Pins example working

--- src/components/pins/Pins.stories.tsx
+++ src/components/pins/Pins.stories.tsx
@@ -1,8 +1,22 @@
-import React from 'react';
-import { Pins } from './Pins';
-
+import React, { useRef } from 'react';
+import { PatchworkPlugin } from '../../canvasPanelComponents/patchwork/src/index';
+import './_pins.scss';
 export default { title: 'Pins Example| Pins' };
 
 export const PinsStory: React.FC = () => {
-  return <Pins />;
+  const pinsEl = useRef('patchwork');
+
+  return (
+    <div className="patchwork-container">
+      <PatchworkPlugin
+        manifest="https://stephenwf.github.io/ocean-liners.json"
+        cssClassMap={{
+          annotation: 'annotation-pin',
+        }}
+        cssClassPrefix="patchwork-"
+        height={500}
+        width={1200}
+      />
+    </div>
+  );
 };
