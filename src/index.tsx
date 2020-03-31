d3a46664dc62f6bb5694b43565e84f0f83295a71
Kelsie Braidwood <33040231+ionab@users.noreply.github.com>
2020-03-31 18:03:07 +0100

    Helper functions

--- src/index.tsx
+++ src/index.tsx
@@ -1,4 +1,64 @@
 import React from 'react';
 import { render } from 'react-dom';
+import { SlideShow } from './viewers/slideshow/index.js';
+import { PopOutViewer } from './viewers/full-page-plugin/index.js';
+import { PatchworkPlugin } from './viewers/patch-work-plugin/src/index.js';
 
-render(<h1>Hello World</h1>, document.getElementById('root'));
+import './viewers/patch-work-plugin/src/index';
+
+// export functions here
+export function createSlideShow(
+  container: HTMLElement,
+  iiifResource: string,
+  addressable?: boolean,
+  backgroundColor?: string,
+  id?: number
+) {
+  render(
+    <SlideShow
+      element={container}
+      manifestUri={iiifResource}
+      addressable={addressable}
+      id={id}
+      backgroundColor={backgroundColor}
+    />,
+    container
+  );
+}
+
+export function createFullScreenTour(
+  container: HTMLElement,
+  iiifResource: string,
+  title: string,
+  innerHtml: string
+) {
+  render(
+    <PopOutViewer
+      manifest={iiifResource}
+      title={title}
+      innerHtml={innerHtml}
+    />,
+    container
+  );
+}
+
+export function createEmbeddedZoom(
+  container: HTMLElement,
+  iiifResource: string,
+  width: number,
+  height: number
+) {
+  render(
+    <PatchworkPlugin
+      manifest={iiifResource}
+      cssClassMap={{
+        annotation: 'annotation-pin',
+      }}
+      cssClassPrefix="patchwork-"
+      height={width}
+      width={height}
+    />,
+    container
+  );
+}
+
