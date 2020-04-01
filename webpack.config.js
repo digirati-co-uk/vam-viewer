c06551067b416c56b533e3cf5e81b09a8dcd8cdb
Stephen Fraser <stephen.fraser@digirati.com>
2020-04-01 08:20:53 +0100

    Bundle preparation

--- webpack.config.js
+++ webpack.config.js
@@ -3,5 +3,12 @@ const config = require('@fesk/scripts/webpack');
 config.externals = {
   'node-fetch': 'fetch',
   'fetch-cookie/node-fetch': 'fetch',
+  openseadragon: 'openseadragon',
+  react: 'react',
+  'react-dom': 'react-dom',
+  'manifesto.js': 'manifesto.jd',
+  'react-flip-move': 'react-flip-move',
+  'react-draggable': 'react-draggable',
 };
+
 module.exports = config;
