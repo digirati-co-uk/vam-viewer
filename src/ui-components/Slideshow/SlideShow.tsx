36fdf31501358a62e400873f78349ac9b73a3303
Kelsie Braidwood <33040231+ionab@users.noreply.github.com>
2020-04-06 12:38:59 +0100

    Capture feedback (#39)
    * Center the video in the canvas
    
    * make video even smaller
    
    * Enable zoom
    
    * Swipe working

--- src/ui-components/Slideshow/SlideShow.tsx
+++ src/ui-components/Slideshow/SlideShow.tsx
@@ -126,8 +126,8 @@ const SlideShow: React.FC<SlideShowProps> = ({
                             onSwipedDown={() => {}}
                             onSwipedUp={() => {}}
                             preventDefaultTouchmoveEvent={true}
-                            trackMouse={false}
-                            trackTouch={false}
+                            trackMouse={true}
+                            trackTouch={true}
                           >
                             <SimpleSlideTransition id={currentIndex}>
                               <Slide
@@ -142,7 +142,6 @@ const SlideShow: React.FC<SlideShowProps> = ({
                                 backgroundColor={backgroundColor}
                                 mobile={qualifiesForMobile}
                                 manifestUri={manifestUri}
-
                               />
                             </SimpleSlideTransition>
                             <CanvasNavigation
