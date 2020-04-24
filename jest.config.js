e4f8ec3e051c3fa3d01bfaa378d04cf7ed3c8e8a
Stephen Fraser <stephen.fraser@digirati.com>
2020-04-24 13:59:12 +0100

    Fixed tests

--- jest.config.js
+++ jest.config.js
@@ -1,17 +1,22 @@
 module.exports = {
-    preset: 'ts-jest/presets/js-with-ts',
-    testEnvironment: 'node',
-    coveragePathIgnorePatterns: [
-        '/node_modules/',
-        '/coverage/',
-        'dist/',
-        '(.test)\\.(ts|tsx|js)$',
-        'jest.transform.js',
-        '.json',
-    ],
-    modulePathIgnorePatterns: ['dist/', 'lib/'],
-    setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'],
-    moduleNameMapper: {
-        '\\.(css|scss)$': '<rootDir>/__mocks__/styleMock.ts',
-    },
+  preset: 'ts-jest/presets/js-with-ts',
+  testEnvironment: 'node',
+  coveragePathIgnorePatterns: [
+    '/node_modules/',
+    '/coverage/',
+    'dist/',
+    '(.test)\\.(ts|tsx|js)$',
+    'jest.transform.js',
+    '.json',
+  ],
+  modulePathIgnorePatterns: ['dist/', 'lib/'],
+  setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
+  moduleNameMapper: {
+    '\\.(css|scss)$': '<rootDir>/__mocks__/styleMock.ts',
+  },
+  globals: {
+    'ts-jest': {
+      tsConfig: './tsconfig.test.json',
+    }
+  }
 };
